1f4b5b8143f52d019fbe91e3748cada2aed6ba70
Michal-K-CB <83695626+Michal-K-CB@users.noreply.github.com>
2021-05-31 08:59:46 +0200

    add new line

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print("Line 2")
+    print("coś tam cośtam")
+    # create dev
 
 
 # Press the green button in the gutter to run the script.
